c59ddfa8ee9769b0b0881c84d645b126826d6471
hitchhooker <tommi@niemi.lol>
2021-03-20 00:18:31 -0300

    add styles to deadend

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,1 +1,5 @@
-<div class="h-screen grid place-items-center text-3xl">Sorry, dead end!</div>
+<div class="h-screen grid place-items-center">    
+    <div class="bg-brand-800 z-50 py-3 px-6 rounded-xl opacity-90">
+    <h1 class="text-3xl text-brand-50">Sorry, it's deadend!</h1>
+    </div>
+</div>
